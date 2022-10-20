c5b62aa4447df85ed6633f13f80fd9e2ca227c86
Sarosh Hassan <91840486+SaroshHassan-github@users.noreply.github.com>
2022-10-20 15:20:30 +0500

    Add files via upload

--- Linked Lists.cpp
+++ Linked Lists.cpp
@@ -1,98 +1,105 @@
 
 #include <iostream>
 using namespace std;
-struct node{
+struct node {
+private:
     int value;
-    node *next;
+public:
+    node* next;
 
     node(int val) {
         value = val;
         next = NULL;
     }
-
+    int getValue() {
+        return value;
+    }
+    node getNext() {
+        return *next;
+    }
 
 };
-//
-//void inserAttail(node*& head, int val) {
+
+//void inserAttail(node* head, int val) {
 //    node* n = new node(val);
-//
-//    if (head == NULL) {
-//        head = n;
-//        return;
-//
-//    }
-//    
 //    node* temp = head;
-//    while (temp->next != NULL) {
+//    while (temp->next!= NULL) {
 //        temp = temp->next;
 //    }
-//    temp->next = n;
+//    temp->next =n;
 //}
-//void insetAthead(node*& head, int val) {
-//    node* n = new node(val);
-//    n->next = head;
-//    head = n;
-//}
+void insetAthead(node*& head, int val) {
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+}
 void disp(node* head) {
     node* temp = head;
     while (temp != NULL) {
-        cout << temp->value<<"--";
+        cout << temp->getValue()<<"--";
         temp = temp->next;
 
     }
     cout << "NULL"<<endl;
 }
-//
-//bool search(node* head, int val) {
-//    node* temp;
-//    temp = head;
-//    
-//    while (temp!= NULL) {
-//        if (temp->value == val) {
-//            return 1;
-//        }
-//
-//        temp = temp->next;
-//  }
-//
-//}
-void delete_Element(node*head, int val) {
-   node *temp;
-   while (temp != head)
-   {while (temp->value==val){ 
-       temp = head;
-    
-       if (temp->value == val) {
-           temp->next = temp->next->next;
-       }
-   
-    }
+
+bool search(node* head, int val) {
+    node* temp = head;
+    while (temp->next == NULL and temp->getValue() == val) {
+        if (temp->getValue() == val)
+        {
+            cout << val << " has been found at " << temp->getValue();
+            return 1;
+        }
+        else
+        {
+            cout << val << " could not be found in Linked List\n";
+            return 0;
+        }
     }
 }
+        
+void delete_Element(node* head) {
+    node* temp=head;
+    while (temp != NULL)
+        disp(head);
 
+
+}
+void printReverse(node* head)
+{
+    // Base case
+    if (head == NULL)
+        return;
+
+    // print the list after head node
+    printReverse(head->next);
+
+    // After everything else is printed, print head
+    cout << head->getValue() << " ";
+}
 int main()
 {
-    /*  node* head = NULL;
-      inserAttail(head, 5);
-      inserAttail(head, 4);
+     node* head = NULL;
+      //inserAttail(head, 5);
+      //inserAttail(head, 4);
       disp(head);
       insetAthead(head,6);
       disp(head);
-      int searchn;
+      /*   int searchn;
       cout << "What number in the LL do you want to search?\n";
       cin >> searchn;
      cout<< search(head, searchn);*/
 
-    node node_A(5);
-    node node_B(3); 
-    node node_C(6);
-    node node_D(2);
-    node node_E(8);
-    node_A.next = &node_B;
-    node_B.next = &node_C;
-    node_C.next = &node_D;
-    node_D.next = &node_E;
-    disp(&node_A);
-    delete_Element(&node_A, 3);
-        disp(&node_B);
+      insetAthead(head, 6);
+      insetAthead(head, 7);
+      insetAthead(head, 9);
+
+
+
+    disp(head);
+    printReverse(head);
+    cout << endl << search(head, 6) << endl;
+
+
 }
